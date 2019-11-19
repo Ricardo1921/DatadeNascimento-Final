ef8694bcdb759191fa40fb27f69b604d30cf6414
Ricardo1921 <49916582+Ricardo1921@users.noreply.github.com>
2019-11-19 16:29:50 +0000

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/DiadaSemana.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp2
+{
+    
+    class DiadaSemana
+    {
+        private DateTime data;
+
+        public string recebe
+        {
+            set
+            {
+                data = Convert.ToDateTime(value);
+            }
+        }
+
+        public string envia
+        {
+            get
+            {
+                return "O dia da semana em que voce nasceu é" + data.ToString("dddd") +".";
+            }
+        }
+       
+
+    }
+}
